bf38867eb003c94126f0e949720c4ed68128d92d
frakery <2606056533@qq.com>
2024-01-11 16:44:20 +0800

    give you a choice

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2 class="title">{{ text }} 她現在{{ weight }} 斤重了！</h2>
+    <button v-on:click="controller">{{ swit }}</button>
   </div>
 </template>
 
@@ -9,16 +10,31 @@
 <script>
 import { ref } from 'vue';
 export default {
-  name:'App',
-  setup(){
+  name: 'App',
+  setup() {
     const text = ref('AYT is a pig')
     const weight = ref(100)
-    setInterval(()=>{
+    const swit = ref('豬豬公主不想被噶，不吃了!')
+    let timer = setInterval(() => {
       weight.value++
-    },1500)
+    }, 1500)
+    function controller() {
+      if (swit.value == '豬豬管不了那麼多了，繼續吃!') {
+        swit.value = '豬豬公主不想被噶，不吃了!'
+        timer = setInterval(() => {
+          weight.value++
+        }, 1500)
+      }
+      else {
+        swit.value = '豬豬管不了那麼多了，繼續吃!'
+        clearInterval(timer)
+      }
+    }
     return {
       text,
-      weight
+      weight,
+      swit,
+      controller
     }
   }
 }
